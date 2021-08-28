9749ae3ed915f7b8b7bd9daeb14e12f28059adf2
Maitreyi1506 <82769270+Maitreyi1506@users.noreply.github.com>
2021-08-29 01:01:27 +0530

    Add files via upload

---
+++ Keywords.py
@@ -0,0 +1,13 @@
+x=5    #an assignment statement
+X=7    #also an assignment statement
+print(x," and ", X)  #case sensitive variables
+sum = x + X + \
+        5 + 10      #line continuation character
+print("Sum is: ",sum)
+y = (5 + 6
+        +7 +9 )     #multiline parentheses
+print("y is : ",y)
+"""For indentation:  
+        (this is a multiline comment)"""
+for i in range(4,16):
+    print("The value is: ",i)
