6d889219d438b0931f160f8c6277da5b8e90025a
Maitreyi1506 <82769270+Maitreyi1506@users.noreply.github.com>
2021-08-29 01:26:12 +0530

    Add files via upload

---
+++ const.py
@@ -0,0 +1,5 @@
+coursename="Object Oriented Programming"
+studentname="Maitreyi"
+score1=92
+score2=95
+score3=88
---
+++ variables and constants.py
@@ -0,0 +1,16 @@
+var1="True"         #String literal
+Var1=True           #Boolean literal
+print("var1: ",var1,"Var1: ",Var1)
+a,b=25,35
+print(a,b)
+a,b= "45"
+print(a,b)
+
+import const
+y=((score1+score2+score3)/3)
+print(y)
+
+a=450           #decimal 
+b=0b1101100     #binary
+c=0o12563721046 #octal
+d=0x12cf        #hexadecimal
